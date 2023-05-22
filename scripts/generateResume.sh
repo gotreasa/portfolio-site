e9c168eb61c4d32a467285da1080f81067df2272
Gearoid O'Treasaigh <gearoid9@gmail.com>
2023-05-22 21:52:07 +0000

    feat: change the generate of the resume to the precommit instead of in the pipeline

--- scripts/generateResume.sh
+++ scripts/generateResume.sh
@@ -5,3 +5,4 @@ mkdir /workspaces/site/latex/generated
 cd /workspaces/site/latex
 xelatex -no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -recorder -output-directory="/workspaces/site/latex/generated"  "/workspaces/site/latex/resume.tex"
 xdvipdfmx -E -o "/workspaces/site/static/files/resume.pdf"  "/workspaces/site/latex/generated/resume.xdv"
+git add /workspaces/site/static/files/resume.pdf
