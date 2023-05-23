0b182bac6b7d58195281f3805415fee5b9613418
Gearoid O'Treasaigh <gearoid9@gmail.com>
2023-05-23 12:19:16 +0000

    chore: change name from site to portfolio-site

--- scripts/generateResume.sh
+++ scripts/generateResume.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-rm /workspaces/site/static/files/resume.pdf
-rm -rf /workspaces/site/latex/generated
-mkdir /workspaces/site/latex/generated
-cd /workspaces/site/latex
-xelatex -no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -recorder -output-directory="/workspaces/site/latex/generated"  "/workspaces/site/latex/resume.tex"
-xdvipdfmx -E -o "/workspaces/site/latex/generated/resume.pdf"  "/workspaces/site/latex/generated/resume.xdv"
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=/workspaces/site/static/files/resume.pdf /workspaces/site/latex/generated/resume.pdf
-git add /workspaces/site/static/files/resume.pdf
+rm /workspaces/portfolio-site/static/files/resume.pdf
+rm -rf /workspaces/portfolio-site/latex/generated
+mkdir /workspaces/portfolio-site/latex/generated
+cd /workspaces/portfolio-site/latex
+xelatex -no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -recorder -output-directory="/workspaces/portfolio-site/latex/generated"  "/workspaces/portfolio-site/latex/resume.tex"
+xdvipdfmx -E -o "/workspaces/portfolio-site/latex/generated/resume.pdf"  "/workspaces/portfolio-site/latex/generated/resume.xdv"
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=/workspaces/portfolio-site/static/files/resume.pdf /workspaces/portfolio-site/latex/generated/resume.pdf
+git add /workspaces/portfolio-site/static/files/resume.pdf
